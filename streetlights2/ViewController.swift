cd6e226d597013c8ef3f22f1b657b56480fb010f
Mariia Berger <slackerpanda@gmail.com>
2021-07-07 10:21:00 +0300

    Initial Commit

---
+++ streetlights2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  streetlights2
+//
+//  Created by Maria Berger on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
